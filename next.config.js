a38443ca6a497d68a9069ebbb682bf8a84a2a2e6
Savio Santos <saviosa08@gmail.com>
2022-06-24 10:19:15 -0300

    Added next headers.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.picsum.photos'],
+  },
+  async headers() {
+    return [{ source: "/(.*)", headers: createSecureHeaders() }];
   },
 }
 
---
+++ pages/404.jsx
@@ -0,0 +1,7 @@
+export default function PageNotFound(){
+    return (
+        <div className="flex justify-center items-center mt-12">
+            <span className="text-2xl text-gray-200">Só existe uma página. A principal.</span>
+        </div>
+    )
+}
